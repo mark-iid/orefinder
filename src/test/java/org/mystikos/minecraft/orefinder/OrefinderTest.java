d626443b425c3f98a3b7411852c9008723ee7d13
Mark <mark@instrumentalid.com>
2025-01-13 21:19:58 -0500

    pom changes and test logging

--- src/test/java/org/mystikos/minecraft/orefinder/OrefinderTest.java
+++ src/test/java/org/mystikos/minecraft/orefinder/OrefinderTest.java
@@ -22,12 +22,16 @@ public class OrefinderTest {
 
     @BeforeEach
     public void setUp() {
+        System.out.println("Setting up server");
         server = MockBukkit.mock();
         server.setMaxPlayers(1);
+        System.out.println("Loading plugin");
         Orefinder orefinder = MockBukkit.load(Orefinder.class);
         PlayerInteractionListener listener = new PlayerInteractionListener(orefinder);
         listener.init();
+        System.out.println("Creating world");
         world = new WorldMock(Material.STONE, 100, 50);
+        System.out.println("Adding player");
         player = server.addPlayer();
     }
 
@@ -38,6 +42,8 @@ public class OrefinderTest {
 
     @Test
     public void testPlayerHitsStoneBlockWithNoDiamond() {
+        System.out.println("testPlayerHitsStoneBlockWithNoDiamond");
+        System.out.println("Player = " + player);
         world.getBlockAt(player.getLocation().add(1, 0, 0)).setType(Material.STONE);
         player.teleport(world.getSpawnLocation());
         // Player holds a diamond
@@ -58,6 +64,8 @@ public class OrefinderTest {
 
     @Test
     public void testPlayerHitsStoneBlockWithDiamond3BlocksAway() {
+        System.out.println("testPlayerHitsStoneBlockWithDiamond3BlocksAway");
+        System.out.println("Player = " + player);
         // Set the diamond ore block 3 blocks away
         world.getBlockAt(player.getLocation().add(1, 0, 0)).setType(Material.STONE);
         world.getBlockAt(player.getLocation().add(3, 0, 0)).setType(Material.DIAMOND_ORE);
@@ -79,6 +87,8 @@ public class OrefinderTest {
 
     @Test
     public void testPlayerHitsStoneBlockWithDiamondOre5BlocksAway() {
+        System.out.println("testPlayerHitsStoneBlockWithDiamondOre5BlocksAway");
+        System.out.println("Player = " + player);
         // Set the diamond ore block 5 blocks away
         world.getBlockAt(player.getLocation().add(1, 0, 0)).setType(Material.STONE);
         world.getBlockAt(player.getLocation().add(5, 0, 0)).setType(Material.DIAMOND_ORE);
@@ -100,6 +110,8 @@ public class OrefinderTest {
 
     @Test
     public void testPlayerHitsBlockWithUnlistedItem() {
+        System.out.println("testPlayerHitsBlockWithUnlistedItem");
+        System.out.println("Player = " + player);
         // Set the diamond ore block 3 blocks away
         world.getBlockAt(player.getLocation().add(1, 0, 0)).setType(Material.STONE);
         world.getBlockAt(player.getLocation().add(3, 0, 0)).setType(Material.DIAMOND_ORE);
